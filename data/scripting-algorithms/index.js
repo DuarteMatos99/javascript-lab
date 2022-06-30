bee10cd692964489f82b78b47d6be110212934b4
DuarteMatos99 <duartematos1999@gmail.com>
2022-06-30 21:56:39 +0100

    Scripting Algorithms added

--- data/scripting-algorithms/index.js
+++ data/scripting-algorithms/index.js
@@ -18,7 +18,7 @@ export const scripting_algorithms = [
             "    return final_sum;\n" +
             "};\n" +
             "\n" +
-            "console.log(sumAll([1, 4])); // 10 \n",
+            "console.log(sumAll([1, 4])); // 10",
     },
     {
         name: "Diff Two Arrays",
@@ -39,7 +39,7 @@ export const scripting_algorithms = [
             "    return final_arr;\n" +
             "};\n" +
             "\n" +
-            "console.log(diffArray([1, 2, 3, 5, 6], [1, 2, 3, 4, 5])); // [6,4]\n",
+            "console.log(diffArray([1, 2, 3, 5, 6], [1, 2, 3, 4, 5])); // [6,4]",
     },
     {
         name: "Seek and Destroy",
@@ -64,7 +64,7 @@ export const scripting_algorithms = [
             "    return arr.filter((item, index) => item != undefined);\n" +
             "};\n" +
             "\n" +
-            "console.log(destroyer([1, 2, 3, 1, 2, 3], 2, 3)); // [1,1]\n",
+            "console.log(destroyer([1, 2, 3, 1, 2, 3], 2, 3)); // [1,1]",
     },
     {
         name: "Wherefore art thou",
@@ -98,7 +98,7 @@ export const scripting_algorithms = [
             "    whatIsInAName([{ apple: 1 }, { apple: 1 }, { apple: 1, bat: 2 }], {\n" +
             "        apple: 1,\n" +
             "    })\n" +
-            "); // [{ apple: 1 }, { apple: 1 }, { apple: 1, bat: 2 }]\n",
+            "); // [{ apple: 1 }, { apple: 1 }, { apple: 1, bat: 2 }]",
     },
     {
         name: "Spinal Tap Case",
@@ -115,6 +115,140 @@ export const scripting_algorithms = [
             "    return lower_str;\n" +
             "};\n" +
             "\n" +
-            'console.log(spinalCase("thisIsSpinalTap")); // this-is-spinal-tap\n',
+            'console.log(spinalCase("thisIsSpinalTap")); // this-is-spinal-tap',
+    },
+    {
+        name: "Pig Latin",
+        description: `<p>Pig Latin is a way of altering English Words. The rules are as follows:</p>
+        <p>- If a word begins with a consonant, take the first consonant or consonant cluster, move it to the end of the word, and add 'ay' to it.</p>
+        <p>- If a word begins with a vowel, just add 'way' at the end.</p>
+        <p>Translate the provided string to Pig Latin. Input strings are guaranteed to be English words in all lowercase.</p>`,
+        code:
+            "\nconst translatePigLatin = (arg) => {\n" +
+            '    const vowels = ["a", "e", "i", "o", "u"];\n' +
+            "    let str = arg.slice();\n" +
+            "    let word = arg.slice();\n" +
+            "    let counter = 0;\n" +
+            "\n" +
+            "    for (let i = 0; i < str.length; i++) {\n" +
+            "        const first_letter = str[i];\n" +
+            "        const is_vowel = vowels.includes(first_letter);\n" +
+            "\n" +
+            "        if (is_vowel) {\n" +
+            '            i == 0 ? (word += "way") : (word += "ay");\n' +
+            "            break;\n" +
+            '        } else if (str.length - 1 == counter) str += "ay";\n' +
+            "\n" +
+            "        word = word.slice(1, word.length) + first_letter;\n" +
+            "        counter += 1;\n" +
+            "    }\n" +
+            "\n" +
+            "    return word;\n" +
+            "};\n" +
+            "\n" +
+            'console.log(translatePigLatin("rhythm")); // rhythmay',
+    },
+    {
+        name: "Search and Replace",
+        description: `<p>Perform a search and replace on the sentence using the arguments provided and return the new sentence.</p>
+        <p>First argument is the sentence to perform the search and replace on.</p>
+        <p>Second argument is the word that you will be replacing (before).</p>
+        <p>Third argument is what you will be replacing the second argument with (after).</p>
+        <p>Note: Preserve the case of the first character in the original word when you are replacing it. For example if you mean to replace the word 'Book' with the word 'dog', it should be replaced as 'Dog'.</p>`,
+        code:
+            "\nconst myReplace = (str, before, after) => {\n" +
+            "    let sentence = str.slice();\n" +
+            "    let replace_word = after.slice();\n" +
+            '    const splited_sentence = sentence.split(" ");\n' +
+            "    const sentence_replaced = splited_sentence.map((word) => {\n" +
+            "        if (word == before) {\n" +
+            "            const first_word_letter = word[0];\n" +
+            "            let first_replace_word_letter = replace_word[0].toLowerCase();\n" +
+            "\n" +
+            "            if (first_word_letter == first_word_letter.toUpperCase()) {\n" +
+            "                first_replace_word_letter =\n" +
+            "                    first_replace_word_letter.toUpperCase();\n" +
+            "            }\n" +
+            "\n" +
+            "            replace_word =\n" +
+            "                first_replace_word_letter +\n" +
+            "                replace_word.slice(1, replace_word.length);\n" +
+            "\n" +
+            "            return replace_word;\n" +
+            "        }\n" +
+            "\n" +
+            "        return word;\n" +
+            "    });\n" +
+            "\n" +
+            '    return sentence_replaced.join(" ");\n' +
+            "};\n" +
+            "\n" +
+            "console.log(\n" +
+            '    myReplace("A quick brown fox Jumped over the lazy dog", "Jumped", "leaped")\n' +
+            "); // A quick brown fox Leaped over the lazy dog",
+    },
+    {
+        name: "DNA Pairing",
+        description: `<p>Pairs of DNA strands consist of nucleobase pairs. Base pairs are represented by the characters AT and CG, which form building blocks of the DNA double helix.</p>
+        <p>The DNA strand is missing the pairing element. Write a function to match the missing base pairs for the provided DNA strand. </p>
+        <p>For each character in the provided string, find the base pair character. Return the results as a 2d array.</p>
+        <p>For example, for the input 'GCG', return '[["G", "C"], ["C","G"], ["G", "C"]]'</p>
+        <p>The character and its pair are paired up in an array, and all the arrays are grouped into one encapsulating array.</p>`,
+        code:
+            "\nconst pairElement = (str) => {\n" +
+            '    const pairs = { A: "T", C: "G", G: "C", T: "A" };\n' +
+            '    const single_genes = str.split("");\n' +
+            "    console.log(single_genes);\n" +
+            "    const dna_pairs = single_genes.map((item) => [item, pairs[item]]);\n" +
+            "\n" +
+            "    return dna_pairs;\n" +
+            "};\n" +
+            "\n" +
+            "console.log(pairElement(\"CGC\")); // [['C', 'G'], ['G', 'C'], ['C', 'G']]",
+    },
+    {
+        name: "Missing letters",
+        description: `<p>Find the missing letter in the passed letter range and return it.</p>
+        <p>If all letters are present in the range, return 'undefined'.</p>`,
+        code:
+            "\nconst fearNotLetter = (str) => {\n" +
+            "    const alpha = Array.from(Array(26)).map((e, i) => i + 65);\n" +
+            "    const alphabet = alpha.map((x) => String.fromCharCode(x).toLowerCase());\n" +
+            "\n" +
+            "    const input_letters = str.slice();\n" +
+            '    const input_array = input_letters.split("");\n' +
+            "    const input_first_index = alphabet.indexOf(input_letters[0]);\n" +
+            "\n" +
+            "    for (let i = 0; i < input_letters.length; i++) {\n" +
+            "        if (input_array[i] != alphabet[input_first_index + i]) {\n" +
+            "            return alphabet[input_first_index + i];\n" +
+            "        }\n" +
+            "    }\n" +
+            "\n" +
+            "    return undefined;\n" +
+            "};\n" +
+            "\n" +
+            'console.log(fearNotLetter("stvwx")); // u',
+    },
+    {
+        name: "Sorted Union",
+        description: `<p>Write a function that takes two or more arrays and returns a new array of unique values in the order of the original provided arrays.</p>
+        <p>In other words, all values present from all arrays should be included in their original order, but with no duplicates in the final array.</p>
+        <p>The unique numbers should be sorted by their original order, but the final array should not be sorted in numerical order.</p>`,
+        code:
+            "\nconst uniteUnique = (...arr) => {\n" +
+            "    const args = [...arr];\n" +
+            "    const unique_arr = [];\n" +
+            "\n" +
+            "    args.forEach((arg) => {\n" +
+            "        arg.forEach((item) => {\n" +
+            "            !unique_arr.includes(item) && unique_arr.push(item);\n" +
+            "        });\n" +
+            "    });\n" +
+            "\n" +
+            "    return unique_arr;\n" +
+            "};\n" +
+            "\n" +
+            "console.log(uniteUnique([1, 2, 3], [5, 2, 1, 4])); // [1, 2, 3, 5, 4]\n",
     },
 ];
--- pages/scripting-algorithms/code.js
+++ pages/scripting-algorithms/code.js
@@ -101,3 +101,141 @@ console.log(
 
 console.log(spinalCase("thisIsSpinalTap")); // this-is-spinal-tap
  */
+
+//Pig Latin
+/* 
+const translatePigLatin = (arg) => {
+    const vowels = ["a", "e", "i", "o", "u"];
+    let str = arg.slice();
+    let word = arg.slice();
+    let counter = 0;
+
+    for (let i = 0; i < str.length; i++) {
+        const first_letter = str[i];
+        const is_vowel = vowels.includes(first_letter);
+
+        if (is_vowel) {
+            i == 0 ? (word += "way") : (word += "ay");
+            break;
+        } else if (str.length - 1 == counter) str += "ay";
+
+        word = word.slice(1, word.length) + first_letter;
+        counter += 1;
+    }
+
+    return word;
+};
+
+console.log(translatePigLatin("rhythm")); // rhythmay
+ */
+
+// Search and Replace
+/* 
+const myReplace = (str, before, after) => {
+    let sentence = str.slice();
+    let replace_word = after.slice();
+    const splited_sentence = sentence.split(" ");
+    const sentence_replaced = splited_sentence.map((word) => {
+        if (word == before) {
+            const first_word_letter = word[0];
+            let first_replace_word_letter = replace_word[0].toLowerCase();
+
+            if (first_word_letter == first_word_letter.toUpperCase()) {
+                first_replace_word_letter =
+                    first_replace_word_letter.toUpperCase();
+            }
+
+            replace_word =
+                first_replace_word_letter +
+                replace_word.slice(1, replace_word.length);
+
+            return replace_word;
+        }
+
+        return word;
+    });
+
+    return sentence_replaced.join(" ");
+};
+
+console.log(
+    myReplace("A quick brown fox Jumped over the lazy dog", "Jumped", "leaped")
+); // A quick brown fox Leaped over the lazy dog
+ */
+
+// DNA Pairing
+/* 
+const pairElement = (str) => {
+    const pairs = { A: "T", C: "G", G: "C", T: "A" };
+    const single_genes = str.split("");
+    console.log(single_genes);
+    const dna_pairs = single_genes.map((item) => [item, pairs[item]]);
+
+    return dna_pairs;
+};
+
+console.log(pairElement("CGC")); //[['C', 'G'], ['G', 'C'], ['C', 'G']]
+ */
+
+// Missing letters
+/* 
+const fearNotLetter = (str) => {
+    const alpha = Array.from(Array(26)).map((e, i) => i + 65);
+    const alphabet = alpha.map((x) => String.fromCharCode(x).toLowerCase());
+
+    const input_letters = str.slice();
+    const input_array = input_letters.split("");
+    const input_first_index = alphabet.indexOf(input_letters[0]);
+
+    for (let i = 0; i < input_letters.length; i++) {
+        if (input_array[i] != alphabet[input_first_index + i]) {
+            return alphabet[input_first_index + i];
+        }
+    }
+
+    return undefined;
+};
+
+console.log(fearNotLetter("stvwx")); //u
+ */
+
+// Sorted Union
+/* 
+const uniteUnique = (...arr) => {
+    const args = [...arr];
+    const unique_arr = [];
+
+    args.forEach((arg) => {
+        arg.forEach((item) => {
+            !unique_arr.includes(item) && unique_arr.push(item);
+        });
+    });
+
+    return unique_arr;
+};
+
+console.log(uniteUnique([1, 2, 3], [5, 2, 1, 4])); // [1, 2, 3, 5, 4]
+ */
+
+// Convert HTML Entities
+
+const convertHTML = (str) => {
+    const html_entities = {
+        "&": "&amp;",
+        "<": "&lt;",
+        ">": "&gt;",
+        "'": "&apos;",
+        '"': "&quot;",
+    };
+    const str_splited = str.slice().split("");
+    const entities = str_splited.map((item, index) => {
+        const char = html_entities[item];
+        if (char != undefined) return char;
+        return item;
+    });
+
+    const final_str = entities.join("");
+    return final_str;
+};
+
+console.log(convertHTML('Stuff in "quotation marks"'));
